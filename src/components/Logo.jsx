06e64b7783d869e297121938b5fb2be912b54b24
anthonyTorres Vargas <anthonytorresvar@hotmail.com>
2021-04-30 13:11:37 -0500

    Updates

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from '../assets/foxbelMusic.png'
-import '../styles/components/logo.css'
-
-export const Logo = () => {
-    return (
-        <img src={logo} className="logo" alt="Logo" />
-    )
-}
